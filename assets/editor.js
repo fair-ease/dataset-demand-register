19dad74f5e25c8926c91f48f879a79d119a66989
Jorge Mendes <jorge.mendes@vliz.be>
2025-02-14 15:19:00 +0100

    Add editor import from URL

--- assets/editor.js
+++ assets/editor.js
@@ -120,37 +120,13 @@ function getParameterDescription(n) {
 function generateTurtle() {
     const name = document.getElementById('name').value
     const description = document.getElementById('description').value
-
-    // https://linkeddata.github.io/rdflib.js/doc/index.html
-    const store = $rdf.graph()
-
-    // const dcat = $rdf.Namespace('http://www.w3.org/ns/dcat#')
-    const rdf = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-    const rdfs = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#')
-    const schema = $rdf.Namespace('http://schema.org/')
-    // const qb = $rdf.Namespace('http://purl.org/linked-data/cube#')
-    const fno = $rdf.Namespace('https://w3id.org/function/ontology#')
-    // const xsd = $rdf.Namespace('http://www.w3.org/2001/XMLSchema#')
+    const queryParams = params.map(p => ({
+        name: getParameterName(p),
+        description: getParameterDescription(p),
+    }))
 
     try {
-        const base = $rdf.Namespace(`${name}#`)
-        const query = $rdf.sym(name)
-
-        store.add(query, rdf('a'), schema('Offer'))
-        store.add(query, rdf('a'), fno('Function'))
-        store.add(query, rdfs('label'), $rdf.literal(description))
-        store.add(query, schema('itemOffered'), $rdf.literal('https://lab.fairease.eu/dataset-demand-register'))
-        store.add(query, fno('expects'),
-            params.map(p => base(`${getParameterName(p)}_param`))
-        )
-        params.forEach(p => {
-            const param = base(`${getParameterName(p)}_param`)
-            store.add(param, rdf('a'), fno('Parameter'))
-            store.add(param, schema('name'), $rdf.literal(getParameterName(p)))
-            store.add(param, rdfs('label'), $rdf.literal(getParameterDescription(p)))
-        })
-
-        return store.serialize(undefined, 'text/turtle')
+        return serialiseAsTurtle({ name, description, params: queryParams })
     } catch (e) {
         return `Error: ${e.message}`
     }
@@ -180,6 +156,53 @@ function copyTurtle() {
 }
 
 
+function parseTurtle(url, input) {
+    const store = $rdf.graph()
+    const rdf = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+    const rdfs = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#')
+    const schema = $rdf.Namespace('http://schema.org/')
+    const fno = $rdf.Namespace('https://w3id.org/function/ontology#')
+
+    $rdf.parse(input, store, url, 'text/turtle')
+
+    const nameNode = store.match(null, rdf('type'), fno('Function'))[0].subject
+    const name = nameNode.value
+    const description = store.match(nameNode, rdfs('label'), null)[0].object
+    const params = store.match(nameNode, fno('expects'), null)
+        .map(p => p.object.elements.map(e => e.value))
+        .flat()
+        .map(p => {
+            const sym = $rdf.sym(p)
+            const nameFact = store.match(sym, schema('name'), null)[0]
+            const name = nameFact ? nameFact.object.value : sym.id()
+            const descriptionFact = store.match(sym, rdfs('label'), null)[0]
+            const description = descriptionFact ? descriptionFact.object.value : ''
+            return { name, description }
+        })
+
+    return {
+        name,
+        description,
+        params,
+    }
+}
+
+
+function editorSetQuery(query) {
+    try {
+        params_seq = 0
+        params = []
+        document.getElementById('name').value = query.name
+        document.getElementById('description').value = query.description
+        query.params.forEach(({ name, description }) => {
+            newParameter(name, description)
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
 function showImport() {
     const dialog = document.getElementById('editor-import')
     dialog.style.display = 'block'
@@ -187,37 +210,9 @@ function showImport() {
 
 function importQuery() {
     clearEditor()
-    try {
-        params_seq = 0
-        params = []
-
-        const store = $rdf.graph()
-        const rdf = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-        const rdfs = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#')
-        const schema = $rdf.Namespace('http://schema.org/')
-        const fno = $rdf.Namespace('https://w3id.org/function/ontology#')
-
-        const content = document.getElementById('editor-import-content').value
-        $rdf.parse(content, store, 'https://lab.fairease.eu/dataset-demand-register/', 'text/turtle')
-
-        const name = store.match(null, rdf('a'), fno('Function'))[0].subject
-        document.getElementById('name').value = name.value
-
-        const description = store.match(name, rdfs('label'), null)[0].object
-        document.getElementById('description').value = description.value
-
-        const ps = store.match(name, fno('expects'), null).map(p => p.object.elements.map(e => e.value)).flat()
-        for (const p of ps) {
-            // try {
-                const sym = $rdf.sym(p)
-                const name = store.match(sym, schema('name'), null)[0].object.value
-                const description = store.match(sym, rdfs('label'), null)[0].object.value
-                newParameter(name, description)
-            // } catch {}
-        }
-    } catch (err) {
-        console.error(err)
-    }
+    const content = document.getElementById('editor-import-content').value
+    const query = parseTurtle('https://lab.fairease.eu/dataset-demand-register/', content)
+    editorSetQuery(query)
     refreshTurtle()
     cancelImport()
 }
@@ -225,4 +220,114 @@ function importQuery() {
 function cancelImport() {
     document.getElementById('editor-import').style.display = 'none'
     document.getElementById('editor-import-content').value = ''
+}
+
+
+function showImportFromURL() {
+    const dialog = document.getElementById('editor-import-url-dialog')
+    dialog.style.display = 'block'
+}
+
+function cancelImportFromURL() {
+    document.getElementById('editor-import-url-dialog').style.display = 'none'
+    document.getElementById('editor-import-url-input').value = ''
+}
+
+
+async function importQueryFromURL() {
+    clearEditor()
+    const url = document.getElementById('editor-import-url-input').value
+    try {
+        const res = await fetch(url)
+        const contentType = res.headers.get('content-type')
+        if (typeof contentType !== 'undefined' && !contentType.match('^\\s*text/turtle\\s*(;.*)?$'))
+            throw Error('unsupported content type "' + contentType + '"')
+        try {
+            const content = await res.text()
+            const query = parseTurtle(url, content)
+            editorSetQuery(query)
+            refreshTurtle()
+            cancelImportFromURL()
+        } catch (e) {
+            console.error(e)
+            alert('Error: see console for details.')
+        }
+    } catch (e) {
+        alert('Error: ' + e.message)
+    }
+}
+
+
+
+/* ===== Turtle ============================================================= */
+
+
+/**
+ * Generate the Turtle representation of the given query.
+ */
+function serialiseAsTurtle({ name, description, params }) {
+    // https://linkeddata.github.io/rdflib.js/doc/index.html
+    const store = $rdf.graph()
+
+    const rdf = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+    const rdfs = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#')
+    const schema = $rdf.Namespace('http://schema.org/')
+    const fno = $rdf.Namespace('https://w3id.org/function/ontology#')
+
+    const base = $rdf.Namespace(`${name}#`)
+    const query = $rdf.sym(name)
+
+    store.add(query, rdf('type'), schema('Offer'))
+    store.add(query, rdf('type'), fno('Function'))
+    store.add(query, rdfs('label'), $rdf.literal(description))
+    store.add(query, schema('itemOffered'),
+        $rdf.literal('https://lab.fairease.eu/dataset-demand-register'))
+    store.add(query, fno('expects'),
+        params.map(p => base(`${p.name}_param`))
+    )
+    params.forEach(p => {
+        const param = base(`${p.name}_param`)
+        store.add(param, rdf('type'), fno('Parameter'))
+        store.add(param, schema('name'), $rdf.literal(p.name))
+        store.add(param, rdfs('label'), $rdf.literal(p.description))
+    })
+
+    return store.serialize(undefined, 'text/turtle')
+}
+
+
+/**
+ * Parse the given Turtle input into a query representation.
+ */
+function parseTurtle(url, input) {
+    const store = $rdf.graph()
+    const rdf = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+    const rdfs = $rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#')
+    const schema = $rdf.Namespace('http://schema.org/')
+    const fno = $rdf.Namespace('https://w3id.org/function/ontology#')
+
+    $rdf.parse(input, store, url, 'text/turtle')
+
+    const nameNode = store.match(null, rdf('type'), fno('Function'))[0].subject
+    const name = nameNode.value
+    const description = store.match(nameNode, rdfs('label'), null)[0].object
+    const params = store.match(nameNode, fno('expects'), null)
+        .map(p => p.object.elements.map(e => e.value))
+        .flat()
+        .map(p => {
+            const sym = $rdf.sym(p)
+            const nameFact = store.match(sym, schema('name'), null)[0]
+            const name = nameFact ? nameFact.object.value : sym.id()
+            const descriptionFact = store.match(sym, rdfs('label'), null)[0]
+            const description = descriptionFact
+                ? descriptionFact.object.value
+                : ''
+            return { name, description }
+        })
+
+    return {
+        name,
+        description,
+        params,
+    }
 }
